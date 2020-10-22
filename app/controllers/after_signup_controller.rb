044886d9108fc8f57fec6f87aeaa089e8d74747a
Philip Oka <philip.oka@gmail.com>
2020-10-22 18:48:08 +0100

    starting the wicked form

---
+++ app/controllers/after_signup_controller.rb
@@ -0,0 +1,3 @@
+class AfterSignupController < Wicked::WizardController
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :after_signup
 
   scope '(:locale)', locale: /en/ do
     root to: 'pages#home'
